055b1cd4dec74503c6dcf1ee7e83a21d1c1deb96
Joksepha <jampotom421@gmail.com>
2023-05-28 08:46:58 +0530

    added existing project files to git

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// es7 react redux extension
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>Hello , world</div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react' //rfce
+import { ReactDOM } from 'react'
+
+function index() {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
