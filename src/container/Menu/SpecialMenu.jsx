5d14360e61736cd8f18cebef0e9b36dc5cd9e7b1
Joksepha <jampotom421@gmail.com>
2023-06-01 07:57:18 +0530

    added upto SpecialMenu and MenuItem

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
 
+import { SubHeading , MenuItem } from '../../components';
+import {  images,data } from '../../constants';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    {/* #Title */}
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits you palatte" />
+      <h1 className="headtext__cormorant">Today's Special</h1>
+    </div>
+
+    {/* Menu */}
+    <div className="app__specialMenu-menu">
+      {/* 1st Menu {Wine}*/}
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu_menu_items">
+          {/* Getting data from MenuItem and passing title,price and tags{wine} */}
+          {data.wines.map(( wine,index ) =>(
+            // <p>{wine.title}</p>
+            <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags}/>
+          ))}
+        </div>
+      </div>
+
+      {/* Image Menu */}
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="menu img" />
+      </div>
+      {/* 2nd Menu */}
+      <div className="app__specialMenu-menu_cocktails flex__center">
+        <p className="app__specialMenu-menu_heading">Cocktails</p>
+        <div className="app__specialMenu_menu_items">
+                    {/* Getting data from MenuItem and passing title,price and tags{cocktails} */}
+          {data.cocktails.map(( cocktail,index ) =>(
+            // <p>{cocktail.title}</p>
+            <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags}/>
+          ))}
+        </div>
+      </div>
+      </div>
+    {/* Button */}
+   <div style={{marginTop:'15px'}}>
+      <button type="button" className="custom__button">View More</button>
+   </div>
   </div>
 );
 
