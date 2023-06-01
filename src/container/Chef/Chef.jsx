9b2beb0ea4e291f6cdd5406eabe2c009fc2f952d
Joksepha <jampotom421@gmail.com>
2023-06-01 09:30:35 +0530

    Added Chef Intro Section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    {/* Chef Image */}
+    <div className="app__wrapper_img app__wrapper_img">
+      <img src={images.chef} alt="chef" />
+    </div> 
+    <div className="app__wrapper_info">
+      {/* Title */}
+        <SubHeading title="Chef's word" />
+        {/*After Title Text*/}
+        <h1 className="headtext__cormorant">What we believe in</h1> 
+
+        <div className="app__chef-content">
+          <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .</p>
+          </div>
+          <p className="p__opensans">auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc.</p>   
+        </div>
+    {/* Chef Info */}
+    <div className="app__chef-sign" >
+      <p>Kevin Luo</p>
+      <p className="p__opensans">Chef & Founder</p>
+      <img src={images.sign} alt="Signature"/>
+    </div>
+  </div>
   </div>
 );
 
